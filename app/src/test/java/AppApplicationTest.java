777b7488eb72881d8f401f5738b8a59a2d757994
tuckerdev <tuckerdev100@outlook.com>
2023-11-08 17:40:21 -0700

    WE HAVE A TEST

--- app/src/test/java/AppApplicationTest.java
+++ app/src/test/java/AppApplicationTest.java
@@ -1,13 +1,13 @@
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AppApplicationTest {
+// @SpringBootTest
+// class AppApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
--- app/src/test/java/WeatherServiceTest.java
+++ app/src/test/java/WeatherServiceTest.java
@@ -1,28 +1,34 @@
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
 import com.weather.app.models.GeocodeData;
 import com.weather.app.services.WeatherService;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(fullyQualifiedNames = "com.weather.app.services.*")
+@PrepareForTest(WeatherService.class)
 public class WeatherServiceTest {
 
     @InjectMocks
-    private WeatherService service;
+    public WeatherService service;
 
     @Before
     public void init() {
         MockitoAnnotations.openMocks(this);
+        Assert.assertNotNull(service);
     }
 
-    public void zipToGeoCodeHappyPath () {
-        //Given
+    @Test
+    public void zipToGeoCodeHappyPath() throws Exception {
+        // Given
         String zipCode = "80303";
         GeocodeData geo = new GeocodeData();
         geo.setLat(0);
@@ -30,10 +36,13 @@ public class WeatherServiceTest {
 
         PowerMockito.mockStatic(WeatherService.class);
         
-        //When
-        when(WeatherService)
-
-        //Then
+        // When
+        WeatherService spy = PowerMockito.spy(service);
+        PowerMockito.doReturn(geo).when(spy, "zipToGeoCode", ArgumentMatchers.any());
+        
+        // Then
+        assertEquals(geo, spy);
     }
-
 }
+
+
